90f950229f504193314545222504f557f81213b3
Andrew Hamlin <andrew@gozynta.com>
2021-01-15 13:31:19 -0500

    run git in async-shell-command with new buffer

--- elisp/init-org.el
+++ elisp/init-org.el
@@ -68,11 +68,12 @@
     (require 'org-tempo))
   (when (file-directory-p "~/org/agenda/")
     (setq org-agenda-files (list "~/org/agenda/")))
-
+  (setq async-shell-command-buffer 'new-buffer)
+ 
   (defun my-git-commit-and-push ()
     "Commit and push changed files in org agenda directory"
-    (shell-command
-     (format "git add -A ~/org/; git commit -m '%s'; git pull -r; git push &"
+    (async-shell-command
+     (format "git add -A ~/org/; git commit -m '%s'; git pull -r; git push;"
              (format-time-string "%Y%m%dT%T"))))
 
   (defun org-export-turn-on-syntax-highlight ()
